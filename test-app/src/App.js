172cfc8697a81720457c46811fde18d2e3a717e1
Andrey Turov <andrey.turov@waveaccess.global>
2024-07-02 00:33:18 +0300

    test AB#17475

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <ul>
+            <li>Testing binding commits to work items</li>
+          </ul>
         </p>
         <a
           className="App-link"
